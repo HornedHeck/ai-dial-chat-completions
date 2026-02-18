1ac025ff9a8517454899dbe163e21a1198c37386
HornedHeck <holitrixpecora@gmail.com>
2026-02-18 15:45:03 +0100

    feat: Implement Dial and Custom clients

--- task/app.py
+++ task/app.py
@@ -8,25 +8,23 @@ from task.models.role import Role
 
 
 async def start(stream: bool) -> None:
-    #TODO:
-    # 1.1. Create DialClient
-    # (you can get available deployment_name via https://ai-proxy.lab.epam.com/openai/models
-    #  you can import Postman collection to make a request, file in the project root `dial-basics.postman_collection.json`
-    #  don't forget to add your API_KEY)
-    # 1.2. Create CustomDialClient
-    # 2. Create Conversation object
-    # 3. Get System prompt from console or use default -> constants.DEFAULT_SYSTEM_PROMPT and add to conversation
-    #    messages.
-    # 4. Use infinite cycle (while True) and get yser message from console
-    # 5. If user message is `exit` then stop the loop
-    # 6. Add user message to conversation history (role 'user')
-    # 7. If `stream` param is true -> call DialClient#stream_completion()
-    #    else -> call DialClient#get_completion()
-    # 8. Add generated message to history
-    # 9. Test it with DialClient and CustomDialClient
-    # 10. In CustomDialClient add print of whole request and response to see what you send and what you get in response
-    raise NotImplementedError
+    dial_client = DialClient("gpt-4")
 
+    conversation = Conversation()
+    conversation.add_message(Message(Role.SYSTEM, DEFAULT_SYSTEM_PROMPT))
+
+    while True:
+        user_input = input().strip()
+        if user_input.lower() == "exit":
+            break
+
+        conversation.messages.append(Message(Role.SYSTEM, user_input))
+
+        if stream:
+            message = await dial_client.stream_completion(conversation.messages)
+        else:
+            message = dial_client.get_completion(conversation.messages)
+        conversation.messages.append(message)
 
 asyncio.run(
     start(True)
--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -1,4 +1,5 @@
 import json
+
 import aiohttp
 import requests
 
@@ -8,7 +9,7 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-class DialClient:
+class ChokingDialClient(BaseClient):
     _endpoint: str
     _api_key: str
 
@@ -17,34 +18,47 @@ class DialClient:
         self._endpoint = DIAL_ENDPOINT + f"/openai/deployments/{deployment_name}/chat/completions"
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and regular response are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #   - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Make POST request using requests.post() with:
-        #   - URL: self._endpoint
-        #   - headers: headers from step 1
-        #   - json: request_data from step 2
-        # 4. Get content from response, print it and return message with assistant role and content
-        # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+        request_data = {"messages": self.__prepare_messages(messages), }
+
+        response = requests.post(url=self._endpoint, headers=self.__headers(), json=request_data)
+
+        if response.status_code != 200:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+        choices = response.json().get("choices", [])
+        if choices:
+            content = choices[0].get("message", {}).get("content")
+            print(content)
+            return Message(Role.AI, content)
+        raise Exception("No choices in response found")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and streamed response chunks are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #    - "stream": True  (enable streaming)
-        #    - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Create empty list called 'contents' to store content snippets
-        # 4. Create aiohttp.ClientSession() using 'async with' context manager
-        # 5. Inside session, make POST request using session.post() with:
-        #    - URL: self._endpoint
-        #    - json: request_data from step 2
-        #    - headers: headers from step 1
-        #    - Use 'async with' context manager for response
-        # 6. Get content from chunks (don't forget that chunk start with `data: `, final chunk is `data: [DONE]`), print
-        #    chunks, collect them and return as assistant message
-        raise NotImplementedError
+        request_data = {"stream": True, "messages": self.__prepare_messages(messages), }
+        contents = []
 
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=self._endpoint, headers=self.__headers(), json=request_data) as response:
+                if response.status == 200:
+                    async for line in response.content:
+                        line_str = line.decode('utf-8').strip()
+                        if line_str.startswith("data: "):
+                            data = line_str[6:].strip()
+                            contents.append(data)
+
+        contents = contents[0:-1]
+        return Message(role=Role.AI, content=''.join(contents))
+
+    @staticmethod
+    def _get_content_snippet(data: str) -> str:
+        data = json.loads(data)
+        if choices := data.get("choices"):
+            delta = choices[0].get("delta", {})
+            return delta.get("content", '')
+        return ''
+
+    @staticmethod
+    def __prepare_messages(messages: list[Message]) -> list[dict[str, str]]:
+        return [message.to_dict() for message in messages]
+
+    def __headers(self) -> dict:
+        return {"api-key": self._api_key, "Content-Type": "application/json"}
